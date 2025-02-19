be99987fffd128a1c7cad0103a1c517d55999b45
Rita <ritaafanaseva@icloud.com>
2025-02-19 19:30:09 +0300

    add last changes

--- js/contacts.js
+++ js/contacts.js
@@ -1,7 +1,12 @@
 const closeContatcs = document.querySelector('.close-contacts');
+const closeContatcsMini = document.querySelector('.close-contacts-change');
+
 const modal = document.getElementById('contacts-wrapper');
 
 console.log(closeContatcs);
 closeContatcs.addEventListener('click', () => {
+    modal.classList.remove('add');
+})
+closeContatcsMini.addEventListener('click', () => {
     modal.classList.remove('add');
 })
